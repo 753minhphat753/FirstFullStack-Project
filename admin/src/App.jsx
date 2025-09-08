0e1602929691b6316cb076e7ebf219b34e0da2c3
Dang Minh Phat <dangminhphatlop10b1@gmail.com>
2025-09-08 21:28:14 +0700

    connect backend with admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
@@ -10,9 +10,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = '$';
 
 const App = () => {
-  const [token, setToken] = useState('');
+  // const [token, setToken] = useState('');
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):''); // Persisting login state using localStorage
+
+  useEffect(()=>{
+    localStorage.setItem('token', token);
+  },[token])
+
 
   return (
     <div className="bg-gray-50 min-h-screen">
@@ -20,15 +27,15 @@ const App = () => {
       {token === "" 
         ? <Login setToken={setToken} />
         : <>
-          <NavBar />
+          <NavBar setToken = {setToken} />
           <hr />
           <div className="flex w-full">
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add />} />
-                <Route path="/list" element={<List />} />
-                <Route path="/orders" element={<Orders />} />
+                <Route path="/add" element={<Add token = {token} />} />
+                <Route path="/list" element={<List token = {token} />} />
+                <Route path="/orders" element={<Orders token = {token} />} />
               </Routes>
             </div>
           </div>
